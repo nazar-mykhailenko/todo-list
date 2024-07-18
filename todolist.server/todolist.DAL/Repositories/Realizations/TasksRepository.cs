790dd4b28d94201462d4ab0b0ecee5ea9cfbfae7
Nazar Mykhailenko <mykhailenkonazar24@gmail.com>
2024-07-18 09:22:14 +0300

    Add solution file

--- todolist.server/todolist.DAL/Repositories/Realizations/TasksRepository.cs
+++ todolist.server/todolist.DAL/Repositories/Realizations/TasksRepository.cs
@@ -10,37 +10,37 @@ public class TasksRepository : ITasksRepository
     private readonly AppDbContext context;
 
     public TasksRepository(AppDbContext context)
-	{
+    {
         this.context = context;
     }
 
     public void Create(TodoTask task)
     {
-		 context.Add(task);
+        context.Add(task);
     }
 
     public void Delete(TodoTask task)
     {
-		context.Remove(task);
+        context.Remove(task);
     }
 
     public async Task<IEnumerable<TodoTask>> GetAll()
     {
-		return await context.Tasks.ToListAsync();
+        return await context.Tasks.ToListAsync();
     }
 
     public async Task<TodoTask> GetById(int id)
     {
-		return await context.Tasks.FirstOrDefaultAsync(t => t.Id == id);
+        return await context.Tasks.FirstOrDefaultAsync(t => t.Id == id);
     }
 
     public async Task Save()
     {
-		await context.SaveChangesAsync();
+        await context.SaveChangesAsync();
     }
 
     public void Update(TodoTask task)
     {
-		context.Update(task);
+        context.Update(task);
     }
 }
